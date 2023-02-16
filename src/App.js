63b54fbe76bea7d91c0e048ec35e52a27e0a1193
Abdullah-Al-Nahian <nahianiiuc@gmail.com>
2023-02-16 14:39:41 +0600

    Signed-off-by: Abdullah-Al-Nahian <nahianiiuc@gmail.com>

--- src/App.js
+++ src/App.js
@@ -37,8 +37,8 @@ function App() {
       <Alert alert={alert}/>
       <div className="container my-3">
         <Routes>
-          <Route path="/about" element={<About />}/>
-          <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below"  mode={mode}/>}/>
+          <Route exact path="/about" element={<About />}/>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below"  mode={mode}/>}/>
         </Routes>
       </div>
     </BrowserRouter>
